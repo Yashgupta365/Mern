f6e17dd2a9a281d49cbcc8f1e68132be7c232add
Yash Gupta <yashgupta@Yashs-MacBook-Air.local>
2021-09-03 00:08:35 +0530

    DB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,26 @@
-const express = require('express')
+import express from 'express'
+import products from  './data/products.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 
-const app = express()
+dotenv.config()
 
-app.listen(5000, console.log('Server Up'))
+connectDB()
+
+const app = express() 
+
+app.get('/', (req,res) => {
+    res.send('API is running')
+})
+
+app.get('/api/products', (req,res) => {
+    res.json(products)
+})
+app.get('/api/products/:id', (req,res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+ 
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
