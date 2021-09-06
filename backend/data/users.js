7591efd69fca9ac1652f89859df08cff710fdb4c
Yash Gupta <yashgupta@Yashs-MacBook-Air.local>
2021-09-06 21:45:15 +0530

    Redux

--- backend/data/users.js
+++ backend/data/users.js
@@ -6,24 +6,21 @@ const users = [
             email: 'admin@example.com',
             password: bcrypt.hashSync('123456', 10),
             isAdmin: true
-
         },
         {
-            name: 'Yash Gupta,
+            name: 'Yash Gupta',
             email: 'yash@example.com',
             password: bcrypt.hashSync('123456', 10),
-            isAdmin: true
+            isAdmin: false
 
         },
         {
             name: 'Manas',
             email: 'Manas@example.com',
-            ppassword: bcrypt.hashSync('123456', 10),
-            isAdmin: true
+            password: bcrypt.hashSync('123456', 10),
+            isAdmin: false
 
         },
 
-
 ]
-
 export default users
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import products from  './data/products.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -13,13 +15,9 @@ app.get('/', (req,res) => {
     res.send('API is running')
 })
 
-app.get('/api/products', (req,res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
  
