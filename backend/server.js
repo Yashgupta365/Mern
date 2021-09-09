2876282162cab840f4357c0535de47d0dc2cef7e
Yash Gupta <yashgupta@Yashs-MacBook-Air.local>
2021-09-09 21:19:31 +0530

    ob dev

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
@@ -14,12 +15,22 @@ const app = express()
 app.use(express.json())
 
 
-app.get('/', (req,res) => {
-    res.send('API is running')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+
+const __dirname = path.resolve()
+
+
+if(process.env.NODE_ENV == 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+}else{
+    app.get('/', (req,res) => {
+    res.send('API is running')
+    })
+}
 
 
 app.use(notFound)
